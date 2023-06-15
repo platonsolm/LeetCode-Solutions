4110c6d6acded8e5cf0bc9b434bb69d931e65aaf
Platon <platon.solovev05@gmail.com>
2023-06-16 01:07:55 +0300

    Update ValidParentheses.py

--- Easy/ValidParentheses.py
+++ Easy/ValidParentheses.py
@@ -3,11 +3,11 @@ class Solution:
         chars, brackets = [], {')': '(', '}': '{', ']': '['}
         for char in s:
             if brackets.__contains__(char):
-                if not chars or chars.pop() != brackets.__getitem__(char):
+                if not chars or chars.pop() != brackets.get(char):
                     return False
             else:
                 chars.append(char)
-        return 0 == len(chars)
+        return len(chars) == 0
 
 
 if __name__ == '__main__':
